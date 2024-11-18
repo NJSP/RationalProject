d26f93ca4dabfda2714f961136649bf44354b189
Nickolas J. Petrone <nickolasjpetrone@outlook.com>
2024-11-18 08:45:19 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RationalProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
