4eb4d0600f95cec5a89cf15fff1ee3ed7b37321e
Nickolas J. Petrone <nickolasjpetrone@outlook.com>
2024-11-18 09:40:58 -0800

    Added WriteRational and Reduce Methods.

--- Program.cs
+++ Program.cs
@@ -5,6 +5,13 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Rational Project");
+            Rational r1 = new Rational();
+            Rational r2 = new Rational(2, 6);
+
+            r2.WriteRational3();
+            r2.Reduce();
+            r2.WriteRational3();
+
         }
     }
 }
